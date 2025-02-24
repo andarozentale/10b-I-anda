639a003938fe8142a74581ba08ae4c8b5befc5b4
andarozentale <andarozentale12@gmail.com>
2025-02-24 11:07:01 +0200

    24.02.2025script.js

---
+++ 21.02.2025/script.js
@@ -0,0 +1,47 @@
+
+let a = 5;
+let b = "5";
+
+if( a == b )
+{ 
+    console.log("a == b");
+}
+if( a === b )
+{
+    console.log("a === b");
+}
+
+
+
+let diena = parseInt(prompt("ievadi dienas numuru"));
+
+switch( diena )
+{
+    case 1:
+        console.log("pirmdiena");
+    break;
+
+    case 2:
+        console.log("otrdiena");
+    break;
+
+    case 3:
+        console.log("trešdiena");
+    break; 
+
+    case 4:
+        console.log("ceturtdiena");
+    break;
+
+    case 5:
+        console.log("piektdiena");
+    break;
+
+    case 6:
+        console.log("sestdiena");
+    break;
+
+    case 7:
+        console.log("svētdiena");
+    break;
+}
