2728a1d4ef017f4cde11ac0b31844dbf1a3eb3b1
andarozentale <andarozentale12@gmail.com>
2025-03-03 11:19:55 +0200

    03.03.2025uzdevumi

---
+++ 03.03.2025/script2.js
@@ -0,0 +1,37 @@
+//1.uzd
+
+let masivs1 = [1, 2, 3, 4, 5,];
+
+for (let i = 0; i < masivs1.length; i++) 
+    { 
+    console.log(masivs1[i]); 
+    } 
+//2.uzd
+let kkk = 0;
+for (let i = 0; i < masivs1.length; i++) 
+    { 
+     if (kkk < masivs1[i] ){
+        kkk = masivs1[i];
+
+     }
+    } 
+    console.log(kkk);
+
+//3.uzd
+let summa = 0;
+for (let i = 0; i <masivs1.length; i++)
+{
+    summa += masivs1[i];
+}
+console.log("elementu summa:", summa);
+
+//4.uzd
+for( let i = 0; i < masivs1.length; i++)
+{
+    if (masivs[i]) 2===0
+    {
+        console.log(masivs1[i])
+    }
+}
+
+
