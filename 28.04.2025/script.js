6dab5caf41911d91e6eb86d00750a05eacf0db00
andarozentale <andarozentale12@gmail.com>
2025-04-28 11:19:35 +0300

    28.04.2025

--- 28.04.2025/script.js
+++ 28.04.2025/script.js
@@ -1,15 +1,31 @@
-function myfunction()
+function myfunction1()
 {
     const nameObj = document.getElementById("user_name");
     const surnameObj = document.getElementById("user_surname");
     const urlObj = document.getElementById("user_url");
 
-    console.log(nameObj.value);
-    console.log(surnameObj.value);
-    console.log(urlObj.value);
+   // console.log(nameObj.value);
+    //console.log(surnameObj.value);
+   // console.log(urlObj.value);
 
-    localStorage.setItem("name", nameObj.value);
-    localStorage.setItem("surname", surnameObj.value);
-    localStorage.setItem("url", urlObj.value);
+   // localStorage.setItem("name", nameObj.value);
+   // localStorage.setItem("surname", surnameObj.value);
+   //localStorage.setItem("url", urlObj.value);
 
+   const user = 
+{
+    name    : nameObj.value,
+    surname : surnameObj.value,
+    url     : urlObj.value
+}
+console.log(user.name);
+localStorage.setItem("user1",JSON.stringify(user));
+
+}
+
+function myfunction2()
+{
+    const user = JSON.parse(localStorage.getItem("user1"));
+    console.log(user);
+    alert(user.name + user.surname + user.url);
 }
